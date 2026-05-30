d254341f59f2ed707b1159827dd497d7c0f61a3e
0xTunay <tigrus232323k@gmail.com>
2026-05-30 11:02:05 +0300

    chore: remove comments

--- Src/main.c
+++ Src/main.c
@@ -6,15 +6,14 @@
 #include "rs485.h"
 
 int main(void) {
-   // clock_init();        /* Configure system to 72 MHz */
+   // clock_init();
     SysTick_Init();
-    RS485_Init();        /* Initialize USART2 + RS-485 */
+    RS485_Init();
 
     USART2_SendString("STM32F411 Modbus PWM Controller\r\n");
     USART2_SendString("System started successfully!\r\n");
 
     while(1) {
-        /* Simple test - echo received data */
         char c = USART2_GetChar();
         USART2_SendChar(c);
 
